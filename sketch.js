553bf07dcea2506cd00e1a0305b235d369614f4d
V-Shake <viviencai6918@gmail.com>
2026-03-31 09:37:48 +0800

    add guide positions for "The Brisk Walker" pose in pose checks

--- sketch.js
+++ sketch.js
@@ -201,6 +201,24 @@ function initializePoseChecks() {
     return;
   }
 
+  if (pose.name === "The Brisk Walker" && currentChecks.length >= 3) {
+    // Brisk Walker guide 1: upper-right arm target.
+    currentChecks[0].guideX = width - 210;
+    currentChecks[0].guideY = 500;
+    currentChecks[0].guideRotationDeg = 80;
+
+    // Brisk Walker guide 2: lower-middle leg target.
+    currentChecks[1].guideX = width - 410;
+    currentChecks[1].guideY = height - 420;
+    currentChecks[1].guideRotationDeg = 335;
+
+    // Brisk Walker guide 3: middle-left arm target.
+    currentChecks[2].guideX = 190;
+    currentChecks[2].guideY = 540;
+    currentChecks[2].guideRotationDeg = 260;
+    return;
+  }
+
   placeGuidesRandomly();
 }
 
